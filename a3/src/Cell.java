ffee5c3f3c1dfcef6a66d989588a1bc91074f4da
William Chen <chenyuwilliam@gmail.com>
2019-11-04 16:02:32 -0500

    A8 ver. 4
    updated assets for game textures

--- a3/src/Cell.java
+++ a3/src/Cell.java
@@ -1,8 +1,23 @@
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+
+import javax.swing.plaf.synth.SynthListUI;
 
 class Cell {
 	static final int size = 20;
+	private Image snakeHead;
+	private Image snakeBody;
+	private Image leftDown;
+	private Image rightDown;
+	private Image leftUp;
+	private Image rightUp;
+	private Image snakeTail;
+	private Image fruit;
+	private Image border;
+	private Image tile;
+
+
 	private boolean isBorder;
 
 	int xPoint;
@@ -17,18 +32,36 @@ class Cell {
 		xPoint = xValue;
 		yPoint = yValue;
 		isBorder = border;
+		this.border = new Image("file:src/assets/border.png", size, size, true, true);
+		this.tile = new Image("file:src/assets/tile.png", size, size, true, true);
+		this.snakeHead = new Image("file:src/assets/snakehead.png", size, size, true, true);
 	}
 
 	void paint(GraphicsContext gc, boolean snake, boolean fruit) {
 		if (snake) {
-			gc.setFill(Color.BLUE);
+			gc.setFill(Color.ROYALBLUE);
+			gc.fillRect(xPoint, yPoint, size, size);
 		} else if (fruit) {
-			gc.setFill(Color.YELLOW);
+			gc.setFill(Color.ORANGE);
+			gc.fillRect(xPoint, yPoint, size, size);
 		} else if (isBorder) {
 			gc.setFill(Color.GREEN);
+			gc.drawImage(border, xPoint, yPoint, size, size);
 		} else {
 			gc.setFill(Color.LIGHTGREEN);
+			gc.drawImage(tile, xPoint, yPoint, size, size);
 		}
-		gc.fillRect(xPoint, yPoint, size, size);
+	}
+
+	void paintHead(GraphicsContext gc, Directions direction) {
+		switch (direction) {
+			case Up:
+				snakeHead.
+		}
+		gc.drawImage();
+	}
+
+	void paintTail(GraphicsContext gc) {
+
 	}
 }
